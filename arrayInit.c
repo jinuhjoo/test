75b9304d6a0232a88c6b052396682a97cb2f66e1
jinuh joo <jinuh@jinuh-jooui-MacBook-Air.local>
2019-02-26 23:36:13 +0900

    arrayInit added

---
+++ arrayInit.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+   int arr1[5]= {1,2,3,4,5};
+   int arr2[]= {1,2,3,4,5,6,7};
+   
+   printf("size of arr1: %lu\n", sizeof(arr1)); 
+   printf("size of arr2: %lu\n", sizeof(arr2));
+
+}
